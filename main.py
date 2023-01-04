3fe749125f0f1d41728968894082451f2be0ab3f
Maya HL <hadjlazibmaya15@gmail.com>
2023-01-04 16:06:58 +0100

    support time filter

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-annots = loadmat('data/stripes.mat')
+annots = loadmat('data/building.mat')
 data = annots['events']
 
 start = time.time()
@@ -22,8 +22,7 @@ def refractory_filter(data, T_ref):
     data_filtered = np.array(data)
     i = 0
     while i < np.shape(data)[0]:
-        k = np.zeros(data_filtered.shape[0])
-        k = k==1
+        k = [False]*data_filtered.shape[0]
         T = T_ref[0]*np.abs(data_filtered[:i, 2] + data_filtered[i, 2])/2 + T_ref[1]*np.abs(data_filtered[:i, 2] - data_filtered[i, 2])/2
         k[:i] = data_filtered[i, 3] - data_filtered[:i, 3] < T
         index_filter = np.where(np.logical_and(data_filtered[k, 0] == data_filtered[i, 0], np.any(data_filtered[k, 1] == data_filtered[i, 1])))[0]
@@ -32,8 +31,26 @@ def refractory_filter(data, T_ref):
         i +=1
     return np.delete(data_filtered, data_filtered[:, 2] == 0, axis= 0)
 
+def support_time_filter(data, T_support, neighboor_size):
+    data_filtered = np.array(data)
+    i = 0
+    for i in range(1, data_filtered.shape[0]):
+        k = [False]*data_filtered.shape[0]
+        k[:i] = data_filtered[i, 3] - data_filtered[:i, 3] < T_support
+        d = (data_filtered[k, 0] - data_filtered[i, 0])**2 + (data_filtered[k, 1] - data_filtered[i, 1])**2
+        index_filter = np.where(np.logical_and(d <= neighboor_size, d != 0))[0]
+        if len(index_filter) == 0:
+            data_filtered[i, :] = [0, 0, 0, 0]
+    return np.delete(data_filtered, data_filtered[:, 2] == 0, axis= 0)
 
-# data = np.array([[1, 2, 1, 1], [1, 2, 1, 20], [1, 2, 1, 21], [1, 2, 1, 80], [1, 2, 1, 101]])
-data_filtered = refractory_filter(data[100:, :], [20, 1])
+def optical_flow():
+    return
+    
+# data = np.array([[1, 2, 1, 1], [2, 2, 1, 15], [2, 1, 1, 21], [8, 8, 1, 23], [1, 1, 1, 30]])
+data_filtered = refractory_filter(data[:100, :], [20, 1])
 
-print(time.time() - start)
+print(data_filtered.shape)
+  
+# print(data[100: , :])
+
+# display_data3D(data[:100, :])
